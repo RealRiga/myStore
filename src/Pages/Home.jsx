8bfa7cdef5b40b887ed483b1d9c706fa65c7fe2f
RealRiga <116930426+RealRiga@users.noreply.github.com>
2023-07-14 18:16:12 -0500

    letter

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
         </div>
         <hr />
         <div className="product-items">
-          <h1>Product</h1>
+          <h1>Products</h1>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-8 md:px-12 lg:px-16 gap-6 mt-5">
             {itemsMap}
           </div>
